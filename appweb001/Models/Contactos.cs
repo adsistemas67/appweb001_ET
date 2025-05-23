8e5203bf916fae2c0c47904592cab69c34990242
EmaEDTT <80005434+EmanuelDTT@users.noreply.github.com>
2025-05-23 09:53:47 -0500

    Cambio sintaxis

--- appweb001/Models/Contactos.cs
+++ appweb001/Models/Contactos.cs
@@ -1,15 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace appweb001.Models
 {
     public class Contactos
     {
+        
         [Key]
         public int id { get; set; }
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public string correoE { get; set; }
-        public DateTime fechaNacimiento { get; set; }
-
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CorreoE { get; set; }
+        public DateTime FechaNacimiento { get; set;}
     }
 }
